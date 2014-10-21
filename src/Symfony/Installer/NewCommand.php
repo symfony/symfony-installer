c7629a1101fb42a8986064926d5aaf75ff00eab6
Javier Eguiluz <javier.eguiluz@gmail.com>
2014-10-21 14:43:13 +0200

    Fixed some minor formatting issue

--- src/Symfony/Installer/NewCommand.php
+++ src/Symfony/Installer/NewCommand.php
@@ -110,7 +110,7 @@ MESSAGE;
         }
 
         if (!preg_match('/^2\.\d\.\d{1,2}$/', $version)) {
-            throw new \RuntimeException("The Symfony version should be 2.N.M, where N = 0..9 and M = 0..99");
+            throw new \RuntimeException('The Symfony version should be 2.N.M, where N = 0..9 and M = 0..99');
         }
 
         // 2.0, 2.1, 2.2 and 2.4 cannot be installed because they are unmaintained
