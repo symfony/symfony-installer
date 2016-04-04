5ef4b775a04406593db28f6f569ae8ed9ec375d0
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-04-04 09:23:17 +0200

    Made all network connections proxy-compatible

--- src/Symfony/Installer/NewCommand.php
+++ src/Symfony/Installer/NewCommand.php
@@ -48,6 +48,7 @@ class NewCommand extends DownloadCommand
     {
         try {
             $this
+                ->checkInstallerVersion()
                 ->checkProjectName()
                 ->checkSymfonyVersionIsInstallable()
                 ->checkPermissions()
